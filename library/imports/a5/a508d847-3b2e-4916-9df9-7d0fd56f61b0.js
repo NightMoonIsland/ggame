d3f1f9658b55b8aa0112349f96103c61682978b1
splspeco <splspeco@spls.com>
2017-10-24 14:57:27 +0800

    haha

---
+++ library/imports/a5/a508d847-3b2e-4916-9df9-7d0fd56f61b0.js
@@ -0,0 +1,28 @@
+"use strict";
+cc._RF.push(module, 'a508dhHOy5JFp35fQ/Vb2Gw', 'urlManager');
+// script/tool/urlManager.js
+
+"use strict";
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,      // The default value will be used only when the component attaching
+        //                           to a node for the first time
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+    },
+
+    // use this for initialization
+    onLoad: function onLoad() {}
+
+});
+
+cc._RF.pop();

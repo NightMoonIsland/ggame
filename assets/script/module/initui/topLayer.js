2b84ac1a81da730fc9064a9cc21c80b65df478ae
Superloserpeco <3323958613@qq.com>
2018-04-25 18:07:02 +0800

    delete unused content

--- assets/script/module/initui/topLayer.js
+++ assets/script/module/initui/topLayer.js
@@ -56,20 +56,6 @@ cc.Class({
             console.log("load simpleTip suceesss");
         });
         this.tipNum = 0;
-
-        // this.node.on('touchend', function(event){
-        //     self.setTouchSwallow(false);
-        //     console.log("touch end");
-        //     var pos = self.node.convertToNodeSpace(event.getLocation())
-        //     var prefab = cc.loader.getRes("ui/click", cc.Prefab);
-        //     if(prefab){
-        //         let item = cc.instantiate(prefab);
-        //         // item.setPosition(event.getLocationX() - 480, event.getLocationY() - 320);
-        //         // self.node.addChild(item);
-        //         item.setPosition(event.getLocationX(), event.getLocationY());
-        //         cc.director.getScene().addChild(item, 5);
-        //     }
-        // }, this);
     },
 
     generateTipNode: function() {
@@ -92,49 +78,6 @@ cc.Class({
     },
 
     addTip: function(tip) {
-
-        // self.tipContainer.tipNum = self.tipContainer.tipNum + 1
-        // local tipNode = cc.CSLoader:createNode("SimpleTip.csb")
-        // tipNode:setCascadeOpacityEnabled(true)
-        // tipNode:getChild("txt"):setString(tip)
-        // tipNode:setPosition(0, (1 - self.tipContainer.tipNum) * 41)
-        // self.tipContainer:addChild(tipNode)
-
-        // local actDisappear = cc.Sequence:create(
-        //     cc.DelayTime:create(1.0),
-        //     cc.FadeOut:create(.5),
-        //     cc.CallFunc:create(function()
-        //         self.tipContainer.tipNum = self.tipContainer.tipNum - 1
-        //         tipNode:removeFromParent()
-        //     end)
-        // )
-        // tipNode:runAction(actDisappear)
-
-
-        // console.log("add tip worked !!!");
-        // var prefab = cc.loader.getRes("ui/simpleTip", cc.Prefab);
-        // this.tipNum = this.tipNum + 1;
-        // if(prefab){
-        //     console.log("generate a new tip");
-        //     let tipNode = cc.instantiate(prefab);
-        //     var label = tipNode.getChildByName("txt").getComponent(cc.Label);
-        //     label.string = tip;
-        //     tipNode.setPosition(0, (1 - this.tipNum) * 41 + 200);
-        //     this.node.addChild(tipNode);
-
-        //     var self = this;
-        //     var actDisappear = cc.sequence(
-        //         cc.delayTime(1.0),
-        //         cc.fadeOut(0.5),
-        //         cc.callFunc(function(){
-        //             console.log("fade out finished");
-        //             self.tipNum = self.tipNum - 1;
-        //             self.tipTrashPool.pushBack(tipNode);
-        //         }, tipNode)
-        //     )
-        //     tipNode.runAction(actDisappear);
-        // }
-
         var tipNode = this.generateTipNode();
         if(tipNode){
             this.tipNum = this.tipNum + 1;
